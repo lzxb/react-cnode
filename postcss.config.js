ca82eadd970f094cc7e3d26d255248386ac8cf96
陈宇仑 <308813800@qq.com>
2017-05-19 14:14:57 +0800

    添加必要文件

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by EX_WLJR_CHENYULUN on 2017/5/18.
+ */
+module.exports = {
+	// parser: 'sugarss',
+	// exec: true,
+	plugins: {
+		'autoprefixer': {}
+	}
+};
---
+++ src/Component/common/getComponent.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class Bundle extends Component {
+	constructor (props){
+		super(props);
+		this.state = {
+			abc:"abc",
+			mod: null
+		};
+	}
+	componentWillMount() {
+		debugger;
+		this.load(this.props)
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.load !== this.props.load) {
+			this.load(nextProps)
+		}
+	}
+
+	load(props) {
+		this.setState({
+			mod: null
+		})
+		props.load().then((mod) => {
+			this.setState({
+				// handle both es imports and cjs
+				mod: mod ? mod.default : mod
+			})
+		}).catch(err => console.log('Failed to load module', err));
+	}
+
+	render() {
+		if (!this.state.mod) return false;
+		return this.props.children(this.state.mod, this.props);
+	}
+}
+
+
+export default function getComponent(props, ComponentFunc) {
+	return (
+		<Bundle load={ComponentFunc} {...props}>
+			{(Module, props) => <Module {...props}/>}
+		</Bundle>
+	)
+}
